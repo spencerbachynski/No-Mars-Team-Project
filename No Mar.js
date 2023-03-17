9cc4213919a96af4a9373347d7ec73f933fc3bc3
spencerbachynski <60793309+spencerbachynski@users.noreply.github.com>
2023-03-17 09:22:07 -0600

    Add files via upload

---
+++ No Mar.js
@@ -0,0 +1,7 @@
+js code:
+
+hamburger = document.querySelector('.hamburger');
+hamburger.onclick = function () {
+    navbar = document.querySelector('.navbar');
+    navbar.classList.toggle('active');
+}
